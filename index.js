eae5060163fc4037fd956a1f26f04377523eed30
Siddhesh More <113719150+Msiddhesh008@users.noreply.github.com>
2023-07-29 20:19:57 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,32 +1,11 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+var prevBtn = document.getElementById('prev')
+var nextBtn = document.getElementById('next')
+var firstImg = document.querySelectorAll('img')[0]
+let carousel = document.querySelector('.carousel')
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-let slides = document.getElementsByClassName('Slider-child');
-  console.log(slides)
-function showSlides(n) {
-  console.log('showslide')
-  let i;
-
-  
-  if(n > slides.length){
-
-    slideIndex = 1
-  }
-
-  if(n < 1){
-    slideIndex = slides.length
-  }
-
-  for (i=0 ; i< slides.length ; i++){
-    slides[i].style.display = 'block';
-  }
-
-  slides[slideIndex-1].style.display = 'flex';
-}
+prevBtn.addEventListener('click', () => {
+  carousel.scrollLeft += -firstImg.clientWidth + 14
+})
+nextBtn.addEventListener('click', () => {
+  carousel.scrollLeft += firstImg.clientWidth + 14
+})
